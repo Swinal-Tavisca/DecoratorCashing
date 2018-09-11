a414bb1895905b292afeae99130db408bfcd56ae
Swinal Kamble <swinal@tavisca.com>
2018-09-11 17:27:57 +0530

    Decorator cashing implemented

---
+++ caching/CachingUsingDecoratorPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IProductService productService = new MemoryCashService();
+            List<Product> products = productService.GetProducts();
+
+            Console.Write("ENTER ID : ");
+            int id = int.Parse(Console.ReadLine());
+            
+            Product product = productService.GetProductById(id);
+
+            Console.WriteLine(product.Id);
+            Console.WriteLine("(ENTER WITHIN 30sec TO GET FROM CACHE)");
+            Console.Write("ENTER ID : ");
+            id = int.Parse(Console.ReadLine());
+            product = productService.GetProductById(id);
+            Console.WriteLine(product.Id);
+            Console.ReadKey();
+
+        }
+    }
+}
